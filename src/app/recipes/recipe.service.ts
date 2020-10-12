a8a8a04dc55b319d9f8e2d22cdb12ab058dd69e6
leandro <carolino.leandro@gmail.com>
2020-10-12 14:50:33 +0200

    service added for cross com component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,8 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A test', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872' ),
     new Recipe('Another test', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872' )
